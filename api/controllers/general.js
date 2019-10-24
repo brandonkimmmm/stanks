d8e251eaf722f4a24bdadfa4c90640babe1dcea1
Brandon Kim <bkim2490@gmail.com>
2019-10-25 00:20:04 +0900

    added eslint

--- api/controllers/general.js
+++ api/controllers/general.js
@@ -3,13 +3,13 @@
 const packageJson = require('../../package.json');
 
 const getHealth = (req, res) => {
-    res.json({
-        name: packageJson.name,
-        version: packageJson.version,
-        basePath: req.swagger.swaggerObject.basePath
-    });
+	res.json({
+		name: packageJson.name,
+		version: packageJson.version,
+		basePath: req.swagger.swaggerObject.basePath
+	});
 };
 
 module.exports = {
-    getHealth
+	getHealth
 }
