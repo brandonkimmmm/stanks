e91b8d5cb1c402e3987bcfc45ca0e4e30a8d9315
Brandon Kim <bkim2490@gmail.com>
2019-11-10 15:06:37 +0900

    getting stats

---
+++ api/helpers/nba.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const nba = require('nba-api-client');
+
+const teamPlayerStats = ()
---
+++ api/helpers/player.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const { Player, Season } = require('../../db/models');
+const nba = require('nba-api-client');
+// const { each } = require('lodash');
+
+const findPlayerStats = (name) => {
+	return Player.findOne({
+		where: {
+			full_name: name
+		},
+		include: {
+			model: Season
+		}
+	})
+		.then((player) => {
+			if (!player) throw new Error('Player not found');
+			let birthday = player.dataValues.dob.substr(4);
+			return findStats(player.Seasons, player.player_id, birthday);
+		})
+		.then((stats) => {
+			console.log('after',stats);
+		})
+}
+
+const findStats = (seasons, playerId, birthday) => {
+	return new Promise((resolve, reject) => {
+		const stats = [];
+		const promises = [];
+		Promise.all([seasons.map((season) => {
+			const date = season.from.toString() + birthday;
+			promises.push(nba.teamPlayerStats({
+				TeamID: season.team_id,
+				Season: season.from_to,
+				SeasonType: 'Regular+Season',
+				DateFrom: date,
+				DateTo: date
+			}));
+		})])
+			.then(() => {
+				return Promise.all(promises);
+			}).then((data) => {
+				return Promise.all([data.map((season) => {
+					const players = season.PlayersSeasonTotals
+					Object.keys(players).map((id) => {
+						if (players[id].PLAYER_ID === playerId) {
+							stats.push(players[id]);
+						}
+					});
+				})]);
+			})
+			.then(() => {
+				resolve(stats);
+			});
+	});
+};
+
+module.exports = {
+	findPlayerStats
+};
